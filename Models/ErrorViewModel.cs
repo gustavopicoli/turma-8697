9babf13a444f545e5fcebcccddb0e0a9416993fd
Gustavo Picoli <gustavo.picoli@hotmail.com>
2023-04-05 19:01:20 -0300

    Criacao de projeto para testar o versionamento

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Versionamento_MO.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
